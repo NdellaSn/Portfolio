78b5eaca47ff677c1373332302f66963a081b8ef
NdellaSn <ndellasene3@gmail.com>
2024-05-06 01:07:57 +0200

    welcome animation plus lien vers reseaux

--- src/components/Welcome/index.jsx
+++ src/components/Welcome/index.jsx
@@ -19,9 +19,15 @@ function Welcome() {
                     <span className='tap-bar'>|</span>
 
                 </h1>
+                <div className='welcome__link' id='welcome__link'>
+                    <a href="https://www.linkedin.com/in/mame-ndella-sene-0123321a5/" target="_blank" rel="noopener noreferrer"><i class="fa-brands fa-linkedin-in"></i></a>
+                    <a href="https://github.com/NdellaSn" target="_blank" rel="noopener noreferrer"><i class="fa-brands fa-github"></i></a>
+                    <a href="mailto:contact@mamendella.fr" target="_blank" rel="noopener noreferrer"><i class="fa-regular fa-envelope"></i></a>
+                    <a href="../../assets/mame-ndella-sene.pdf" download="mame-ndella-sene.pdf"><i class="fa-regular fa-file-pdf"></i></a>
+                </div>
             </div>
             <div className='welcome__down__page'>
-            <i class="fa-solid fa-arrow-down"></i>
+                <i class="fa-solid fa-arrow-down"></i>
             </div>
             <div className='welcome__cover'></div>
         </div>
@@ -37,8 +43,11 @@ const text = ["Je suis Mame Ndella SENE.", "Je suis Développeuse Frontend."]
 
 function type() {
     const phrase = text[indexPhrase];
-    if (indexPhrase >= text.length)
+    if (indexPhrase >= text.length) {
+        document.getElementById("welcome__link").style.transform = "translateY(0px)";
+        document.getElementById("welcome__link").style.opacity = 1;
         return
+    }
     if (indexPhrase === 0 && index === 0) {
         document.getElementById("presentation").innerHTML = "";
     }
